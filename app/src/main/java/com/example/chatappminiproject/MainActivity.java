51a64ad61d9b580df1b44712586bf10b2b8cf96b
Jiating98 <tingchong0519@gmail.com>
2021-05-30 04:29:49 +0800

    LoginRegister

--- app/src/main/java/com/example/chatappminiproject/MainActivity.java
+++ app/src/main/java/com/example/chatappminiproject/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.chatappminiproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.btnlogin);
+        register = findViewById(R.id.btnregister);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent2login = new Intent(MainActivity.this, Login.class);
+                startActivity(intent2login);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent2register = new Intent(MainActivity.this, Register.class);
+                startActivity(intent2register);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/chatappminiproject/User.java
@@ -0,0 +1,18 @@
+package com.example.chatappminiproject;
+
+public class User {
+
+    public String userName, Phone, Email, Password;
+
+    public User(){
+
+    }
+
+    public User(String username, String phone, String email, String password){
+        this.userName = username;
+        this.Phone = phone;
+        this.Email = email;
+        this.Password = password;
+    }
+
+}
