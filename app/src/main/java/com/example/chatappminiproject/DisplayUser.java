0e816fd9117371658115ed03682001eba79cb81b
foojingying <minozjingying98@gmail.com>
2021-05-31 23:11:26 +0800

    JYcommit

---
+++ app/src/main/java/com/example/chatappminiproject/DisplayUser.java
@@ -0,0 +1,14 @@
+package com.example.chatappminiproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_user);
+    }
+}
