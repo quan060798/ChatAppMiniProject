7c35eb5f2f09ee8267465907af2139453a0787d2
foojingying <minozjingying98@gmail.com>
2021-06-06 17:13:18 +0800

    Get Location

--- app/src/main/java/com/example/chatappminiproject/MessageActivity.java
+++ app/src/main/java/com/example/chatappminiproject/MessageActivity.java
@@ -43,7 +43,7 @@ public class MessageActivity extends AppCompatActivity {
     FirebaseUser fuser;
     DatabaseReference reference;
 
-    ImageButton btn_send;
+    ImageButton btn_send, btn_gogetlocation;
     EditText text_send;
     String useridforlist;
 
@@ -64,6 +64,7 @@ public class MessageActivity extends AppCompatActivity {
         username = findViewById(R.id.other_username);
         btn_send = findViewById(R.id.btn_send);
         text_send = findViewById(R.id.ed_sendtext);
+        btn_gogetlocation = findViewById(R.id.btn_gogetlocation);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
@@ -97,6 +98,14 @@ public class MessageActivity extends AppCompatActivity {
                     Toast.makeText(MessageActivity.this, "You can't send empty message", Toast.LENGTH_SHORT).show();
                 }
                 text_send.setText("");
+            }
+        });
+
+        btn_gogetlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2gogetlocat = new Intent(MessageActivity.this,GetLocation.class);
+                startActivity(intent2gogetlocat);
             }
         });
 
